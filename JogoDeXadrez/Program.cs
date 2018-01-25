8198cee1e358d0cb845938e9470eb9e0cc28b6c5
José Eronaldo Marques Filho <eronaldo.marques96@gmail.com>
2018-01-25 00:19:55 -0200

    implementacao do Tabuleiro

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace JogoDeXadrez
 {
@@ -7,11 +7,7 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args)
         {
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-            Console.WriteLine(P);
+            Tabuleiro t = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
